d8b2b69cc908926be3142980064c02a7f6265fa3
Seema-Fahmawe <seemafahmawe2000@gmail.com>
2023-02-05 19:48:18 +0300

    add portfolio page

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,4 @@
 /* loading header*/
-
 let carousel = document.querySelector(".carousel");
 carousel.style.opacity = 0;
 document.querySelector(".loading").style.visibility = "visible";
@@ -13,11 +12,10 @@ window.addEventListener("load", function () {
 })
 
 /* header scroll */
-
 let caption = document.querySelectorAll(".carousel-caption");
 window.addEventListener("scroll", function () {
     caption.forEach((caption) => {
-        if (window.scrollY > 54) {
+        if (window.scrollY > 60) {
             caption.style.transform = "translateY(220px)";
             caption.style.opacity = 0;
             caption.style.transition = "all 0.7s";
@@ -30,4 +28,54 @@ window.addEventListener("scroll", function () {
 
 })
 
+
+
+/* scroll to top */
+document.querySelector(".top-icon").style.opacity = 0;
+window.addEventListener("scroll", function () {
+    let newProductPos = this.document.querySelector(".newProduct").offsetTop;
+    if (window.scrollY > newProductPos) {
+        document.querySelector(".top-icon").style.opacity = 1;
+        document.querySelector(".top-icon").style.transition = "0.7s";
+    } else {
+        document.querySelector(".top-icon").style.opacity = 0;
+        document.querySelector(".top-icon").style.transition = "0.7s";
+    }
+})
+
+let topbtn = document.querySelector(".top");
+topbtn.addEventListener("click", function () {
+    window.scroll({
+        top: 0,
+        behaviour: "smooth",
+    })
+})
+
+
+
+
+let prevScrollpos = window.pageYOffset;
+let navLink = document.querySelector(".nav-link::after");
+
+window.onscroll = function () {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+        if (scrollY > 70) {
+            document.querySelector(".navbar-brand").style.marginTop = "-12px";
+            document.querySelector(".navbar-brand").style.marginBottom = "-12px";
+            for (let i = 0; i < navLink.length; i++) {
+                navLink.style.bottom = "0px ";
+            }
+        } else {
+
+            document.querySelector(".navbar-brand").style.marginTop = "0";
+            document.querySelector(".navbar-brand").style.marginBottom = "0";
+        }
+
+    } else {
+        document.getElementById("navbar").style.top = "-90px";
+    }
+    prevScrollpos = currentScrollPos;
+}
 
