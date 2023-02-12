e2d7dcc36f4ff2d9fe90062766df9c9d167ebbbc
israa-yahya <israayahya23@gmail.com>
2023-02-12 15:10:09 +0200

    israa

--- assets/script/scriptLogIn.js
+++ assets/script/scriptLogIn.js
@@ -1,21 +1,33 @@
-
 /* scroll to top */
 document.querySelector(".top-icon").style.opacity = 0;
 window.addEventListener("scroll", function () {
-    let loginPos = this.document.querySelector(".login").offsetTop;
-    if (window.scrollY > loginPos) {
-        document.querySelector(".top-icon").style.opacity = 1;
-        document.querySelector(".top-icon").style.transition = "0.7s";
-    } else {
-        document.querySelector(".top-icon").style.opacity = 0;
-        document.querySelector(".top-icon").style.transition = "0.7s";
-    }
-})
+  let loginPos = this.document.querySelector(".login").offsetTop;
+  if (window.scrollY > loginPos) {
+    document.querySelector(".top-icon").style.opacity = 1;
+    document.querySelector(".top-icon").style.transition = "0.7s";
+  } else {
+    document.querySelector(".top-icon").style.opacity = 0;
+    document.querySelector(".top-icon").style.transition = "0.7s";
+  }
+});
 
 let topbtn = document.querySelector(".top");
 topbtn.addEventListener("click", function () {
-    window.scroll({
-        top: 0,
-        behaviour: "smooth",
-    })
-})
+  window.scroll({
+    top: 0,
+    behaviour: "smooth",
+  });
+});
+/* end nav */
+/* start login*/
+
+var exampleInputEmail1 = document.getElementById("exampleInputEmail1");
+var exampleInputPassword1 = document.getElementById("exampleInputPassword1");
+var exampleInputEmail2 = document.getElementById("exampleInputEmail2");
+var exampleInputEmail2 = document.getElementById("exampleInputEmail2");
+
+function adduser(e) {
+e.preventDefault();
+window.console.log(exampleInputEmail1.value ,exampleInputPassword1.value );
+}
+
